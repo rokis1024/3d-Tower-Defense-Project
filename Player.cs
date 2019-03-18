67ea9b181d9f8b42687519b79d3fca014cbb6572
rokis1024 <47740331+rokis1024@users.noreply.github.com>
2019-03-18 11:34:57 +0200

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int hp = 2;
+    int money;
+    int income;
+
+    public void lastWaypoint()
+    {
+        hp--;
+        if (hp <= 0)
+            Debug.Log("Player lost");
+    }
+}
---
+++ WaveSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+
+    public Transform enemyPrefab;
+    public Transform spawner;
+
+    public float waveTimer = 5f;
+    public float countDown = 2f;
+    public float timeBetweenEnemies = 0.5f;
+    private int waveNumber = 1;
+
+    void Update()
+    {
+        if (countDown <= 0)
+        {
+            StartCoroutine(SpawnWave());
+            countDown = waveTimer;
+        }
+        else
+            countDown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave ()
+    {
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(timeBetweenEnemies);
+        }
+        waveNumber++;
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawner.position, spawner.rotation);
+    }
+
+}
