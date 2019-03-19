506947957fe570143210fc6c42d02d07232797c0
rokis1024 <47740331+rokis1024@users.noreply.github.com>
2019-03-19 19:08:43 +0200

    Add files via upload

---
+++ Assets/Bullet.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    private GameObject target;
+    private int towerDamage;
+
+    public float speed = 70f;
+
+    public void getInfo(GameObject enemy, int damage)
+    {
+        target = enemy;
+        towerDamage = damage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Vector3 dir = target.transform.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if(dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+
+    }
+
+    void HitTarget()
+    {
+        Enemy enemy = target.GetComponent<Enemy>();
+        enemy.takeDamage(towerDamage);
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Enemy:MonoBehaviour
+{
+
+    public string name = "Warrior";
+    public int health = 1;
+
+    public void takeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+            Destroy(gameObject);
+
+    }
+    
+}
